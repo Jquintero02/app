18fd056c711be72476049537b94ba24db11efd81
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-08-27 20:55:38 -0500

    three layer completed with test to client

--- src/test/java/com/grandma/app/clients/controller/ClientsControllerTest.java
+++ src/test/java/com/grandma/app/clients/controller/ClientsControllerTest.java
@@ -1,26 +1,93 @@
 package com.grandma.app.clients.controller;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import com.grandma.app.clients.dto.ClientDto;
+import com.grandma.app.clients.service.ClientsService;
 
 class ClientsControllerTest {
+    @Mock
+    private ClientsService clientsService;
+    private ClientsController clientsController;
+
+    @BeforeEach
+    void setUp() {
+        clientsController = new ClientsController(clientsService);
+    }
 
     @Test
     void createClient() {
+        Mockito.doReturn(Optional.of(new ClientDto())).when(clientsService).createClient(Mockito.any(ClientDto.class));
+
+        ClientDto clientDto = new ClientDto(
+                "CC-1",
+                "Juan",
+                "juan@gmail.com",
+                "3128283889",
+                "cl 9a");
+
+        clientsController.createClient(clientDto);
+
+        Assertions.assertThat(clientsController.createClient(clientDto)).isNotNull();
+
+        Mockito.verify(clientsService).createClient(Mockito.any(ClientDto.class));
     }
 
     @Test
     void getClient() {
+        Mockito.doReturn(new ClientDto()).when(clientsService).getClient(Mockito.anyString());
+
+        clientsController.getClient("CC-1");
+
+        Assertions.assertThat(clientsController.getClient("CC-1")).isNotNull();
+
+        Mockito.verify(clientsService).getClient(Mockito.anyString());
     }
 
     @Test
     void updateClient() {
+        Mockito.doNothing().when(clientsService).updateClient(Mockito.anyString(), Mockito.any(ClientDto.class));
+
+        ClientDto clientDto = new ClientDto(
+                "CC-1",
+                "Felipe",
+                "felipe@gmail.com",
+                "3128283889",
+                "cl 9a");
+
+        clientsController.updateClient("CC-1", clientDto);
+
+        Assertions.assertThat(clientsController.updateClient("CC-1", clientDto)).isNotNull();
+
+        Mockito.verify(clientsService).updateClient(Mockito.anyString(), Mockito.any(ClientDto.class));
     }
 
     @Test
     void deleteClient() {
+        Mockito.doNothing().when(clientsService).deleteClient(Mockito.anyString());
+
+        clientsController.deleteClient("CC-1");
+
+        Assertions.assertThat(clientsController.deleteClient("CC-1")).isNotNull();
+
+        Mockito.verify(clientsService).deleteClient(Mockito.anyString());
     }
 
     @Test
     void getOrderClients() {
+        Mockito.doReturn(Optional.of(new ClientDto())).when(clientsService).getOrderClients(Mockito.anyString(),
+                Mockito.anyString());
+
+        clientsController.getOrderClients("name", "ASC");
+
+        Assertions.assertThat(clientsController.getOrderClients("name", "ASC")).isNotNull();
+
+        Mockito.verify(clientsService).getOrderClients(Mockito.anyString(), Mockito.anyString());
     }
 }
--- src/test/java/com/grandma/app/clients/repository/ClientsRepositoryTest.java
+++ src/test/java/com/grandma/app/clients/repository/ClientsRepositoryTest.java
@@ -1,14 +1,33 @@
 package com.grandma.app.clients.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Sort;
 
 class ClientsRepositoryTest {
+    private ClientsRepository clientsRepository = Mockito.mock(ClientsRepository.class);
 
     @Test
     void findByDocument() {
+        Mockito.doReturn(null).when(clientsRepository).findByDocument(Mockito.anyString());
+
+        clientsRepository.findByDocument("CC-1");
+
+        Assertions.assertThat(clientsRepository.findByDocument("CC-1")).isNull();
+
+        Mockito.verify(clientsRepository).findByDocument(Mockito.anyString());
+
     }
 
     @Test
     void findAll() {
+        Mockito.doReturn(null).when(clientsRepository).findAll(Sort.by(Sort.Direction.ASC, "name"));
+
+        clientsRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+
+        Assertions.assertThat(clientsRepository.findAll()).isNull();
+
+        Mockito.verify(clientsRepository).findAll(Sort.by(Sort.Direction.ASC, "name"));
     }
 }
